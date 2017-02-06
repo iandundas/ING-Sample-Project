ef67ba5a7f3d16db1f643cdbb205de480fef032b
Ian Dundas <contact@iandundas.co.uk>
2017-02-06 19:17:23 +0000

    Added Currency, PaymentAccount & SavingsAccount model objects

---
+++ Model/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  Bankieren
+//
+//  Created by Ian Dundas on 06/02/2017.
+//  Copyright © 2017 Ian Dundas. All rights reserved.
+//
+
+import Foundation
+
+public enum Currency: String {
+    case eur = "EUR"
+    case unknown = ""
+}
