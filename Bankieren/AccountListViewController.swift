470fb6375926cd05f94f1fac51d2a8e26c37b8f7
Ian Dundas <contact@iandundas.co.uk>
2017-02-06 16:09:10 +0000

    Added skeleton for AccountListViewController

---
+++ Bankieren/AccountListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AccountListViewController.swift
+//  Bankieren
+//
+//  Created by Ian Dundas on 06/02/2017.
+//  Copyright © 2017 Ian Dundas. All rights reserved.
+//
+
+import Foundation
+
+protocol AccountListViewModelType{
+    
+}
+
+class AccountListViewController: BaseBoundViewController<AccountListViewModelType>  {
+    
+}
