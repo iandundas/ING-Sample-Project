24c6d853bfb33b7793b655849f6397f0168373d1
Ian Dundas <contact@iandundas.co.uk>
2017-02-07 13:27:00 +0000

    Added UserInterface framework target

---
+++ UserInterface/UserInterface.h
@@ -0,0 +1,19 @@
+//
+//  UserInterface.h
+//  UserInterface
+//
+//  Created by Ian Dundas on 07/02/2017.
+//  Copyright © 2017 Ian Dundas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserInterface.
+FOUNDATION_EXPORT double UserInterfaceVersionNumber;
+
+//! Project version string for UserInterface.
+FOUNDATION_EXPORT const unsigned char UserInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserInterface/PublicHeader.h>
+
+
